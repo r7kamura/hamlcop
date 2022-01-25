db5d4475b1aad0492be5e5c8f08ee050b6f3454b
Ryo Nakamura <r7kamura@gmail.com>
2022-01-25 09:29:06 +0900

    bundle gem hamlcop

---
+++ lib/hamlcop.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "hamlcop/version"
+
+module Hamlcop
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/hamlcop_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Hamlcop do
+  it "has a version number" do
+    expect(Hamlcop::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
